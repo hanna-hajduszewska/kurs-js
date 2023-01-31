0b72bdfb23ba1299cd574d8288e872240ef75a85
Hanna Hajduszewska <hanna.hajduszewska@gmail.com>
2023-01-31 16:12:42 +0100

    loop created in app.js

--- app.js
+++ app.js
@@ -1,15 +1,7 @@
 let activeSlideNumber = 1;
-
 let arrowLeft = document.querySelector('.arrow-left');
 let arrowRight = document.querySelector('.arrow-right');
 
-let dot1 = document.querySelector('#dot1');
-let dot2 = document.querySelector('#dot2');
-let dot3 = document.querySelector('#dot3');
-
- let slide1 = document.querySelector('#slide1');
- let slide2 = document.querySelector('#slide2');
- let slide3 = document.querySelector('#slide3');
 
  let hideActiveSlide = () => {
     let activeElement = document.querySelector('.active');
@@ -24,8 +16,7 @@ let dot3 = document.querySelector('#dot3');
  let showNextSlide = () => {
     if(activeSlideNumber === 3) {
         activeSlideNumber = 1;
-    }
-    else {
+    } else {
         activeSlideNumber = activeSlideNumber + 1;
     }
     showSlide(activeSlideNumber);
@@ -34,30 +25,19 @@ let dot3 = document.querySelector('#dot3');
  let showPreviousSlide = () => {
     if(activeSlideNumber === 1) {
         activeSlideNumber = 3;
-    }
-    else {
+    } else {
         activeSlideNumber = activeSlideNumber - 1;
     }
     showSlide(activeSlideNumber);
  }
 
-let showSlide1 = () => {
-    activeSlideNumber = 1;
-    showSlide(1);
+for(let i = 1; i <= 3; i++) {
+    let showSlideI = () => {
+        activeSlideNumber = i;
+        showSlide(i);  
+    };
+    document.querySelector('#dot'+i).addEventListener('click', showSlideI);
 }
-
-let showSlide2 = () => {
-    activeSlideNumber = 2;
-    showSlide(2);
-};
-
-let showSlide3 = () => {
-    activeSlideNumber = 3;
-    showSlide(3);
-}
-dot1.addEventListener('click', showSlide1);
-dot2.addEventListener('click', showSlide2);
-dot3.addEventListener('click', showSlide3);
 
 arrowRight.addEventListener('click', showNextSlide);
 arrowLeft.addEventListener('click', showPreviousSlide);
--- examples.js
+++ examples.js
@@ -1,20 +1,7 @@
-let bool = 5 > 10;
-console.log(bool);
-
-let bool2 = 1 === 1;
-console.log(bool2);
-
-let bool3 = 1 !== 2;
-console.log(bool3);
-
-let isBiggerThan10 = (number) => {
-    if(number > 10) {
-        console.log('Liczba jest większa od 10');
-        }
-    if(number <= 10) {
-        console.log('Liczba nie jest większa od 10');
-    }    
+let circleLength = (radius) => {
+    console.log(2*radius*3.14);
 }
 
-isBiggerThan10(20);
-isBiggerThan10(5);
+for(let i = 1; i <= 20; i++) {
+    circleLength(i);
+}
